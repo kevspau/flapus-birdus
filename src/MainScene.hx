48e48ef6a54276875a76aad2bf0f08ce85659f92
sharpcdf <csharpdfjer@gmail.com>
2023-01-23 17:03:58 -0500

    add rotations and bg

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -2,18 +2,26 @@ import ceramic.ScanCode;
 import ceramic.Scene;
 import ceramic.Key;
 import ceramic.Text;
-
+import ceramic.Quad;
 import Player;
 
 class MainScene extends Scene {
     var plr: Player;
     override function preload() {
         assets.add(Images.PLAYERSHEET);
+        assets.add(Images.FLAPUS_BACKGROUND);
     }
     override function create() {
+        var bg = new Quad();
+        bg.texture = assets.texture(Images.FLAPUS_BACKGROUND);
+        bg.width = width;
+        bg.height = height;
+        add(bg);
+
         plr = new Player();
         plr.pos(width/4, height/2);
         add(plr);
+
         input.onKeyDown(this, onDown);
         input.onKeyUp(this, onUp);
     }
@@ -23,21 +31,33 @@ class MainScene extends Scene {
             app.arcade.world.isPaused = true;
             app.scenes.main = new DeathScreen(this);
         }
+        //rotates bird based on state
+        if (plr.smach.state == FLYING && plr.rotation > -20) {
+            plr.rotation -= 50 * d;
+        } else if (plr.smach.state == FALLING && plr.rotation < 20) {
+            plr.rotation += 50 * d;
+        }
     }
     function onDown(key:Key) {
         //if space is pressed then flapus starts going up
         if (key.scanCode == ScanCode.SPACE) {
-            plr.animation = "flying";
-            plr.gravityY = 0;
-            plr.accelerationY = -500;
+            if (plr.smach.state == FALLING) {
+                plr.animation = "flying";
+                plr.gravityY = 0;
+                plr.accelerationY = -500;
+                plr.smach.state = FLYING;
+            }
         }
     }
     function onUp(key:Key) {
         //once space is released then gravity is reset for flapus
         if (key.scanCode == ScanCode.SPACE) {
-            plr.animation = "falling";
-            plr.gravityY = 600;
-            plr.accelerationY = 0;
+            if (plr.smach.state == FLYING) {
+                plr.animation = "falling";
+                plr.gravityY = 600;
+                plr.accelerationY = 0;
+                plr.smach.state = FALLING;
+            }
         }
     }
 }
--- src/Player.hx
+++ src/Player.hx
@@ -1,8 +1,15 @@
+import ceramic.StateMachine;
 import ceramic.Sprite;
 import ceramic.SpriteSheet;
 
+
+enum abstract State(Int) {
+    var FALLING;
+    var FLYING;
+}
 class Player extends Sprite {
     var scene = app.scenes.main;
+    @component public var smach = new StateMachine<State>();
     public function new() {
         super();
         initArcadePhysics();
@@ -14,6 +21,6 @@ class Player extends Sprite {
         sheet.addGridAnimation("flying", [1], 0);
         quad.anchor(1, 0.5);
         animation = "falling";
-
+        smach.state = FALLING;
     }
 }
