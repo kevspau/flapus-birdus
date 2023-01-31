8ceaf06f236e12e12fedc75f3eb95e98c9fd6c4d
sharpcdf <csharpdfjer@gmail.com>
2023-01-30 21:16:54 -0500

    add collision to base pipe class

--- src/Pipe.hx
+++ src/Pipe.hx
@@ -15,26 +15,41 @@ class Pipe extends Visual {
         initArcadePhysics();
         immovable = true;
         allowGravity = false;
-        head = new Quad();
-        head.initArcadePhysics();
-
-        tail = new Quad();
-        tail.initArcadePhysics();
-
-        head.texture = headtex;
-        tail.texture = tailtex;
-        anchor(0.5, 0.5);
-        head.anchor(0.5, 1);
-        tail.anchor(0.5, 1);
 
         var tailLength = Std.random(cast scene.height / tailtex.height - 10);
+        size(headtex.width, tailtex.height * (tailLength + 5) + headtex.height);
+
+        var q = new Quad();
+        q.color = ceramic.Color.RED;
+        q.size(arcade.body.width, arcade.body.height);
+        q.pos(arcade.body.x, arcade.body.y);
+        q.depth = 100;
+        add(q);
+        
+        tail = new Quad();
+        tail.anchor(0.5, 1);
+        tail.pos(width/2, height);
+        tail.scaleY = tailLength + 5;
+        tail.texture = tailtex;
+        add(tail);
+
+        head = new Quad();
+        head.anchor(0.5, 1);
+        head.pos(width/2, height - tailtex.height * (tailLength + 5));
+        head.texture = headtex;
+        add(head);
+        
+        anchor(0.5, 1);
+        /*head.anchor(0.5, 1);
+        tail.anchor(0.5, 1);
+
         tail.scaleY = tailLength + 5; //+ 5 keeps the minimum length at 5 in case tailLength = 0
         tail.x = width/2;
         head.pos(width/2, height - tailtex.height * (tailLength + 5)); //puts it at the end of the tail
 
         
         add(head);
-        add(tail);
+        add(tail);*/
 
         //log.debug(tailLength);
 
